bf301a8765d5de6e0f18d1393e32913c6969884b
heeman74 <heeman0918@gmail.com>
2021-09-25 08:25:20 -0700

    finished BE API

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,99 @@
+import { AddressBook } from './AddressBook';
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
 
-const app  = express();
 
-// app.get('/', (req, res) => res.send("HELLO"));
+const app = express();
+const jsonParser = bodyParser.json();
 
-app.use(express.static(path.resolve(__dirname,  '..', '../client/dist')));
-// console.log(path.resolve())
-// console.log(__dirname)
-app.get('/', (_, res) => {
-  res.sendFile(path.resolve('/Users/heechung/address-book/address-book/src/client/dist/index.html'));
+const addressBook = new AddressBook();
+app.use(express.static(path.resolve(__dirname, '..', '../client/dist')));
+
+
+
+app.get('/contacts/paginated', async (req, res) => {
+	try{
+		const {page, itemsPerPage} = req.query;
+		const contacts = await addressBook.getContactsPaginated(+page, +itemsPerPage);
+		res.status(200).send(contacts)
+	}catch(error){
+		res.status(500).send('Internal Error');
+	}
 })
-app.listen(3000, () => console.log("server is listening on port 3000"))
+
+app.get('/contacts/:id', async(req, res) => {
+	try {
+		const contact = await addressBook.getContact(req.params.id)
+		res.status(200).send(contact);
+	} catch(error){
+		if (error instanceof Error) {
+			res.status(404).send({
+				statusCode: 404,
+				message: error.message,
+				error: 'Not Found'
+			})
+		} else {
+			res.status(500).send('Internal Error');
+		}
+	}
+})
+
+app.delete('/contacts/:id', async(req, res) => {
+	try{
+		await addressBook.deleteContact(req.params.id)
+	}catch(error){
+		if (error instanceof Error) {
+			res.status(404).send({
+				statusCode: 404,
+				message: error.message,
+				error: 'Not Found'
+			})
+		} else {
+			res.status(500).send('Internal Error');
+		}
+	}
+})
+
+app.put('/conacts/:id', jsonParser, async(req, res) => {
+	try {
+		const contact = req.body;
+		const updatedContact = await addressBook.updateContact(req.params.id, contact)
+		res.status(200).send(updatedContact);
+	} catch(error){
+		if (error instanceof Error) {
+			res.status(404).send({
+				statusCode: 404,
+				message: error.message,
+				error: 'Not Found'
+			})
+		} else {
+			res.status(500).send('Internal Error');
+		}
+	}
+})
+
+app.post('/contacts', jsonParser, async(req, res) => {
+	try{
+		const contact = req.body;
+		console.log(req.body)
+		const addedConact = await addressBook.addContact(contact);
+		res.status(201).send(addedConact)
+	}catch(error){
+		console.log(error);
+		res.status(501).send('Internal Error');
+	}
+})
+
+app.get('/', (_, res) => {
+	res.sendFile(path.resolve(__dirname, '..', '../client/dist/index.html'));
+});
+
+const startServer = async () => {
+	try {
+		app.listen(3000, () => console.log('server is listening on port 3000'));
+	} catch (error) {
+		console.log(error);
+	}
+};
+startServer()
